a55e951b11fdb98044178780a087d2a4194c91dd
Muhammad Tahir Sundhu <114790426+MuhammadTahirSundhu@users.noreply.github.com>
2024-08-08 03:43:21 +0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  base: '/', // Set this if your app is deployed at the root of your domain
+});
